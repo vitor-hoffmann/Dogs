99b5c0bc7b6fe2e42fd3cd94e9c65cca8a16506b
Vitor Valerio Hoffmann <vitorvaleriohoffmann@gmail.com>
2024-07-05 09:21:31 -0300

    feat: Estrutura inicial do projeto

---
+++ src/Components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <nav className="container">
+        <Link to="/">
+          <Dogs />
+        </Link>
+        <Link to="/login">Login | Criar</Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
