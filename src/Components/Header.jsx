7133f1dffd97799f7bb227329bd42043a4baf875
Vitor Valerio Hoffmann <vitorvaleriohoffmann@gmail.com>
2024-07-08 08:11:14 -0300

    feat: implementado o header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
-import { ReactComponent as Dogs } from "../Assets/dogs.svg";
+import Dogs from "../Assets/dogs.svg?react";
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav className="container">
-        <Link to="/">
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" className={styles.logo} aria-label="Dogs - Home">
           <Dogs />
         </Link>
-        <Link to="/login">Login | Criar</Link>
+        <Link to="/login" className={styles.login}>
+          Login | Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
