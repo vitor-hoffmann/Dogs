fbf3c28e1083c852429d636aba61e62367eb496c
Vitor Valerio Hoffmann <vitorvaleriohoffmann@gmail.com>
2024-07-10 09:26:14 -0300

    chore: ajustes finais

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -6,6 +6,7 @@ import useFetch from '../../Hooks/useFetch';
 import { PASSWORD_RESET } from '../../Api';
 import Error from '../Helper/Error';
 import { useNavigate } from 'react-router-dom';
+import Head from '../Helper/Head';
 
 const LoginPasswordReset = () => {
   const [login, setLogin] = React.useState('');
@@ -36,7 +37,8 @@ const LoginPasswordReset = () => {
   }
 
   return (
-    <div>
+    <section className="animeLeft">
+      <Head title="Resete a senha" />
       <h1 className="title">Resete a Senha</h1>
       <form onSubmit={handleSubmit}>
         <Input
@@ -52,7 +54,7 @@ const LoginPasswordReset = () => {
         )}
       </form>
       <Error error={error} />
-    </div>
+    </section>
   );
 };
 
